e8241450561df09d2c7613a3ddb41970f47e3b49
Nikita Kuzmin <guidemaster228@gmail.com>
2025-04-13 15:37:20 +0500

    fix: model name length

--- src/app/models/organization.py
+++ src/app/models/organization.py
@@ -12,7 +12,7 @@ class Organization(Base):
     repr_cols = ("id", "name")
 
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid7)
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(String(64), nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=False)
     is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
     deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
