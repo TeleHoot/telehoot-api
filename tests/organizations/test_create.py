033b5a3961c8b2ad0b47977c7f0b4b0d09e1fcf0
Илья <128465818+Karasik876@users.noreply.github.com>
2025-04-17 21:33:44 +0500

    chore: assert actually created in DB

--- tests/organizations/test_create.py
+++ tests/organizations/test_create.py
@@ -1,11 +1,15 @@
 import httpx
 import pytest
 from fastapi import status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.app import models
 
 pytestmark = pytest.mark.asyncio(loop_scope="session")
 
 
-async def test_create_organizations_success(client: httpx.AsyncClient):
+async def test_create_organizations_success(client: httpx.AsyncClient, db_session: AsyncSession):
     org_data = {"name": "Tralalello Tralala"}
     response: httpx.Response = await client.post("/organizations/", json=org_data)
 
@@ -14,6 +18,11 @@ async def test_create_organizations_success(client: httpx.AsyncClient):
 
     assert response_data.get("name") == org_data["name"]
     assert "id" in response_data
+
+    org_id = response_data["id"]
+    org = await db_session.get(models.Organization, org_id)
+    assert org is not None
+    assert org.name == org_data["name"]
 
 
 @pytest.mark.parametrize(
@@ -47,7 +56,7 @@ async def test_read_organizations_empty_db(client: httpx.AsyncClient):
     assert response.json() == []
 
 
-async def test_read_many_organizations(client: httpx.AsyncClient):
+async def test_read_many_organizations(client: httpx.AsyncClient, db_session: AsyncSession):
     num_created, org_data = 3, {"name": "TestName"}
     for _ in range(num_created):
         await client.post("/organizations/", json=org_data)
@@ -57,3 +66,6 @@ async def test_read_many_organizations(client: httpx.AsyncClient):
     assert response.status_code == status.HTTP_200_OK
 
     assert len(response.json()) == num_created
+
+    result = await db_session.scalars(select(models.Organization))
+    assert len(result.all()) == num_created
