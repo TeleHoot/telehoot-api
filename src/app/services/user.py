6d74fb781f99049935dcb1fe9e9b0c7f2d183037
Nikita Kuzmin <guidemaster228@gmail.com>
2025-04-18 22:33:31 +0500

    fix: types

--- src/app/services/user.py
+++ src/app/services/user.py
@@ -16,5 +16,5 @@ class Users(
             update_schema=schemas.users.Create,
         )
 
-    def read_by_tg_id(self):
+    def read_by_tg_id(self, uow, tg_id):
         raise NotImplementedError
