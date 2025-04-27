e0c33e45652b6c96372884328652665d60e6eca8
Илья <128465818+Karasik876@users.noreply.github.com>
2025-04-28 00:26:26 +0500

    chore: check_none_tuple validator

--- src/core/schemas/filter.py
+++ src/core/schemas/filter.py
@@ -2,7 +2,7 @@ import enum
 from datetime import datetime
 from typing import Annotated
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class SortOrderField(enum.StrEnum):
@@ -18,3 +18,10 @@ class PaginationParams(BaseModel):
 class BaseFilters(BaseModel):
     created_at: tuple[datetime | None, datetime | None] | None = None
     updated_at: tuple[datetime | None, datetime | None] | None = None
+
+    @field_validator("created_at", "updated_at", mode="after")
+    @classmethod
+    def check_none_tuple(cls, value: tuple | None):
+        if isinstance(value, tuple) and all(v is None for v in value):
+            return None
+        return value
