361607d4afcd9a0bb653d6cda8a7564a7b41b567
Илья <128465818+Karasik876@users.noreply.github.com>
2025-04-30 19:52:30 +0500

    fix: tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -55,16 +55,16 @@ async def test_auth_new_user(
 
     # Check that user was created in the database
     user: models.User | None = await db_session.scalar(
-        select(models.User).where(models.User.telegram_id == fake_telegram_data["telegram_id"])
+        select(models.User).where(models.User.telegram_id == fake_telegram_data["id"])
     )
 
     assert user is not None
-    assert user.telegram_username == fake_telegram_data["telegram_username"]
+    assert user.telegram_username == fake_telegram_data["username"]
 
     # Check that organization was created in the database
     org: models.Organization | None = await db_session.scalar(
         select(models.Organization).where(
-            models.Organization.name == fake_telegram_data["telegram_username"].capitalize()
+            models.Organization.name == fake_telegram_data["username"].capitalize()
         )
     )
     assert org is not None
