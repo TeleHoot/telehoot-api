43d7f5941022ecaf167ef6ee9437e0d40da7a9d5
Yurchenko Stanislav <yurchenko.stas15@gmail.com>
2025-05-19 19:09:17 +0500

    tests: Added tests for sessions (update, delete)

--- tests/sessions/test_basic.py
+++ tests/sessions/test_basic.py
@@ -21,6 +21,21 @@ async def test_create_session(
     assert session_data["status"] == models.SessionStatus.WAITING
 
 
+async def test_get_session(
+    user_client: AsyncClient,
+    organization: models.Organization,
+    quiz: models.Quiz,
+    session: models.Session,
+):
+    response = await user_client.get(
+        f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions/{session.id}"
+    )
+    assert response.status_code == status.HTTP_200_OK
+    session_data = response.json()
+    assert session_data["id"] == str(session.id)
+    assert session_data["hosts"][0]["role"] == "host"
+
+
 async def test_get_sessions(
     user_client: AsyncClient,
     organization: models.Organization,
@@ -31,7 +46,48 @@ async def test_get_sessions(
         f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions"
     )
     assert response.status_code == status.HTTP_200_OK
-    sessions = response.json()
-    assert len(sessions) == 1
-    assert sessions[0]["id"] == str(session.id)
-    assert sessions[0]["hosts"][0]["role"] == "host"
+    sessions_data = response.json()
+    assert len(sessions_data) == 1
+    assert sessions_data[0]["id"] == str(session.id)
+    assert sessions_data[0]["hosts"][0]["role"] == "host"
+
+
+async def test_update_session(
+    user_client: AsyncClient,
+    organization: models.Organization,
+    quiz: models.Quiz,
+    session: models.Session,
+):
+    response = await user_client.patch(
+        f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions/{session.id}",
+        json={"status": "active"},
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    response = await user_client.get(
+        f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions/{session.id}"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    session_data = response.json()
+    assert session_data["status"] == "active"
+
+
+async def test_delete_session(
+    user_client: AsyncClient,
+    organization: models.Organization,
+    quiz: models.Quiz,
+    session: models.Session,
+):
+    response = await user_client.delete(
+        f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions/{session.id}",
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+    response = await user_client.get(
+        f"/organizations/{organization.id}/quizzes/{quiz.id}/sessions/{session.id}"
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
